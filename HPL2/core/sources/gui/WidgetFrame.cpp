60331dddeda87efb96a4b422edb1e64e65fc75ad
jkulawik <darkfire.9251@gmail.com>
2020-10-02 00:10:29 +0200

    An almost working fix

--- HPL2/core/sources/gui/WidgetFrame.cpp
+++ HPL2/core/sources/gui/WidgetFrame.cpp
@@ -96,6 +96,13 @@ namespace hpl {
 	// PUBLIC METHODS
 	//////////////////////////////////////////////////////////////////////////
 
+
+	void cWidgetFrame::ChangeBackgroundForColorPicking()
+	{
+		mpGfxBackground = mpSkin->GetGfx(eGuiSkinGfx_FrameBackground); //eGuiSkinGfx_FrameBackgroundColorPicking
+		//Need to find a way how to put a custom thingy here^
+	}
+
 	//-----------------------------------------------------------------------
 
 	void cWidgetFrame::OnAttachChild(iWidget* apChild)
