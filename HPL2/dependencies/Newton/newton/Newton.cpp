da35ec5e375a966e59a1e7b6364abcd846d746f1
Vasily Postnicov <shamaz.mazum@gmail.com>
2021-09-12 17:55:51 +0300

    Shit, constructing dgVector from an array has a different behavior

--- HPL2/dependencies/Newton/newton/Newton.cpp
+++ HPL2/dependencies/Newton/newton/Newton.cpp
@@ -86,10 +86,10 @@ void SaveCollision (const NewtonCollision* collisionPtr)
 #endif
 
 static dgMatrix array2matrix(const dFloat *array) {
-	dgVector row1(&array[0]);
-	dgVector row2(&array[4]);
-	dgVector row3(&array[8]);
-	dgVector row4(&array[12]);
+	dgVector row1(array[0],  array[1],  array[2],  array[3]);
+	dgVector row2(array[4],  array[5],  array[6],  array[7]);
+	dgVector row3(array[8],  array[9],  array[10], array[11]);
+	dgVector row4(array[12], array[13], array[14], array[15]);
 	dgMatrix mat(row1, row2, row3, row4);
 
 	return mat;
